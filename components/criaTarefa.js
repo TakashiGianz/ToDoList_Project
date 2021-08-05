4b61a3a002ef9f1345e56391d9c6c1e52339d1f3
Fernando-Escritorio <fernando.takashi12@gmail.com>
2021-08-04 21:28:31 -0300

    Finishing Tasks

--- components/criaTarefa.js
+++ components/criaTarefa.js
@@ -12,10 +12,12 @@ export const handleNovoItem = (evento) => {
   const data = moment(calendario.value);
 
   const dataFormatada = data.format("DD/MM/YYYY HH:mm");
+  const concluida = false;
 
   const dados = {
     valor,
     dataFormatada,
+    concluida
   };
 
   const tarefasAtualizadas = [...tarefas, dados];
@@ -26,14 +28,19 @@ export const handleNovoItem = (evento) => {
   carregaTarefa();
 };
 
-export const Tarefa = ({ valor, dataFormatada }) => {
+export const Tarefa = ({ valor, dataFormatada , concluida}, id) => {
   const tarefa = document.createElement("li");
-  tarefa.classList.add("task");
+
   const conteudo = `<p class="content"> ${dataFormatada} * ${valor}</p>`;
+
+  if(concluida){
+    tarefa.classList.add("done");
+  }
+  tarefa.classList.add("task");
 
   tarefa.innerHTML = conteudo;
 
-  tarefa.appendChild(BotaoConclui());
+  tarefa.appendChild(BotaoConclui(carregaTarefa, id));
   tarefa.appendChild(BotaoDeleta());
 
   return tarefa;
